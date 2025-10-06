58ddfc57e4d61a61c16c9631d7be365f63ad6f0d
Nicolas Zelarayan <nicolaszelarayan2003@gmail.com>
2025-10-06 12:33:55 -0300

    Update SocioPerfilPage.jsx

--- defensa-gym/src/pages/PAGINAS DE SOCIO/SocioPerfilPage.jsx
+++ defensa-gym/src/pages/PAGINAS DE SOCIO/SocioPerfilPage.jsx
@@ -1,5 +1,5 @@
-import { Footer } from "../../../../../Nueva carpeta (4)/src/components/HEADER Y FOOTER/Footer"
-import { Header } from "../../../../../Nueva carpeta (4)/src/components/HEADER Y FOOTER/Header"
+import { Footer } from "../../components/HEADER Y FOOTER/Footer"
+import { Header } from "../../components/HEADER Y FOOTER/Header"
 import { Perfil } from "../../components/MAIN/Perfil";
 
 export const SocioPerfilPage = () => {
